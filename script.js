9954c2ce7d75f33ad559a90cab50c8d7d7ad8a10
MAT-2 <mpcasey30@gmail.com>
2023-09-29 14:40:27 -0500

    Experimenting and in progress creating a timer.

---
+++ script.js
@@ -0,0 +1,6 @@
+var startButton = document.querySelector("#startButton");
+var seconds = 60;
+
+startButton.addEventListener("click", function () {
+  var start = setInterval(() => {}, interval);
+});
